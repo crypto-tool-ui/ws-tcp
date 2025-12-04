eb6426092001b1d1fa7cbc71d6c6dc0e72d49b30
crypto-tool-ui <joniiie1456@gmail.com>
2025-12-04 21:49:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { TcpProxy } = require("./lib_dev.js");
 
 (() => {
   new TcpProxy(
-    "stratum+tcp://us2.salvium.herominers.com:1230",
-    "SC1siHCYzSU3BiFAqYg3Ew5PnQ2rDSR7QiBMiaKCNQqdP54hx1UJLNnFJpQc1pC3QmNe9ro7EEbaxSs6ixFHduqdMkXk7MW71ih",
+    "stratum+tcp://de.salvium.herominers.com:1230",
+    "SC1siHCYzSU3BiFAqYg3Ew5PnQ2rDSR7QiBMiaKCNQqdP54hx1UJLNnFJpQc1pC3QmNe9ro7EEbaxSs6ixFHduqdMkXk7MW71ih.001=240000",
     "x",
     {
       port: 8000,
